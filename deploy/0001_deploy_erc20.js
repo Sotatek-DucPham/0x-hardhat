a54c4cc62a3a74df1ce0044c678f037cbb16926b
Sotatek-DucPham <duc.pham@sotatek.com>
2021-05-21 14:17:54 +0700

    feat: contract

--- deploy/0001_deploy_erc20.js
+++ deploy/0001_deploy_erc20.js
@@ -2,14 +2,7 @@ const func = async function ({ deployments, getNamedAccounts }) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const decimals = 18;
-  const token = await deploy("Token", {
-    from: deployer,
-    args: ["TOKEN", "TOK", 18, deployer, `1000000000${"0".repeat(decimals)}`],
-    log: true,
-  });
-
-  const deposit = await deploy("Deposit", {
+  await deploy("ZRXToken", {
     from: deployer,
     args: [],
     log: true,
