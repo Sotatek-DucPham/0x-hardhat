8f0d0c8f166b154e6f8258bd83fa52632eefbd69
Sotatek-DucPham <duc.pham@sotatek.com>
2021-05-24 18:20:22 +0700

    f

--- deploy/0001_deploy_asset_wrapper.js
+++ deploy/0001_deploy_asset_wrapper.js
@@ -9,26 +9,26 @@ const func = async function ({ deployments, getNamedAccounts, getChainId }) {
     args: [],
     log: true,
   });
-  await deploy("ERC721Proxy", {
+  const erc721Proxy = await deploy("ERC721Proxy", {
     from: deployer,
     args: [],
     log: true,
   });
 
-  await deploy("DummyERC20Token", {
+  const zrxToken = await deploy("DummyERC20Token", {
     from: deployer,
     args: ["0x Protocol Token", "ZRX", 18, "1000000000000000000000000000"],
     log: true,
   });
 
-  await deploy("WETH9", {
+  const etherToken = await deploy("WETH9", {
     from: deployer,
     args: [],
     log: true,
   });
 
   const chainId = await getChainId();
-  await deploy("Exchange", {
+  const exchangeTx = await deploy("Exchange", {
     from: deployer,
     args: [chainId],
     log: true,
@@ -37,7 +37,7 @@ const func = async function ({ deployments, getNamedAccounts, getChainId }) {
   // Dummy ERC20 tokens
   for (const token of erc20TokenInfo) {
     const totalSupply = "1000000000000000000000000000";
-    await deploy("DummyERC20Token", {
+    const dummyErc20Token = await deploy("DummyERC20Token", {
       from: deployer,
       args: [token.name, token.symbol, token.decimals.toString(), totalSupply],
       log: true,
@@ -68,6 +68,20 @@ const func = async function ({ deployments, getNamedAccounts, getChainId }) {
     args: [],
     log: true,
   });
+
+  const erc20Proxy = await deployments.get("ERC20Proxy");
+  console.log(erc20Proxy);
+  await erc20Proxy.addAuthorizedAddress(exchangeTx.address);
+
+  // await erc721Proxy
+  //   .addAuthorizedAddress(exchange.address)
+  //   .awaitTransactionSuccessAsync(txDefaults);
+  // await erc1155Proxy
+  //   .addAuthorizedAddress(exchange.address)
+  //   .awaitTransactionSuccessAsync(txDefaults);
+  // await multiAssetProxy
+  //   .addAuthorizedAddress(exchange.address)
+  //   .awaitTransactionSuccessAsync(txDefaults);
 };
 
 module.exports = func;
