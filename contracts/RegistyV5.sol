56b76cdc8c6068dc0279f24be5523b4aaaf52c90
Sotatek-DucPham <duc.pham@sotatek.com>
2021-05-24 09:56:20 +0700

    f

---
+++ contracts/RegistyV5.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.5;
+
+contract RegistryV5 {}
--- deploy/0001_deploy_asset_wrapper.js
+++ deploy/0001_deploy_asset_wrapper.js
@@ -7,6 +7,17 @@ const func = async function ({ deployments, getNamedAccounts }) {
     args: [],
     log: true,
   });
+  await deploy("ERC721Proxy", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
+
+  await deploy("DummyERC20Token", {
+    from: deployer,
+    args: ["0x Protocol Token", "ZRX", 18, "1000000000000000000000000000"],
+    log: true,
+  });
 };
 
 module.exports = func;
